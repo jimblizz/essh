cc06df506e2fe33ecd7dd44244907a50b5009178
James Blizzard <james.blizzard@browserlondon.com>
2018-12-24 12:33:44 +0000

    Using interactive shell session

--- src/shell.go
+++ src/shell.go
@@ -4,9 +4,11 @@ import (
     "fmt"
     "github.com/tidwall/gjson"
     "golang.org/x/crypto/ssh"
+    "io"
     "io/ioutil"
     "net"
     "os"
+    "sync"
     "time"
 )
 
@@ -41,9 +43,6 @@ func shellOpenSession (ps PathStructure) {
 }
 
 func sshSession (c ContainerDigest) {
-
-    //spew.Dump(c)
-
     // Start an SSH session
     // TODO: Hardcoded for testing, this will be different per users, and thus will need to be configured some place
     sshKeyLocation := "/var/application/browserinfrastructure/keys"
@@ -82,22 +81,60 @@ func sshSession (c ContainerDigest) {
     // TODO: Multiple commands: https://stackoverflow.com/questions/24440193/golang-ssh-how-to-run-multiple-commands-on-the-same-session
     //shellExec(session, "docker ps -q")
     //shellExec(session, "docker ps -a")
-    containerId := shellGetDockerId(session, c)
-    fmt.Println(containerId)
+    //containerId := shellGetDockerId(session, c)
+    //fmt.Println(containerId)
 
     // TODO: We need to be using an interactive SSH session to proceed
+
+    modes := ssh.TerminalModes{
+        ssh.ECHO:          0,     // disable echoing
+        ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+        ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+    }
+
+    if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+        panic(err)
+    }
+
+    w, err := session.StdinPipe()
+    if err != nil {
+        panic(err)
+    }
+    r, err := session.StdoutPipe()
+    if err != nil {
+        panic(err)
+    }
+    in, out := MuxShell(w, r)
+    if err := session.Start("/bin/sh"); err != nil {
+        panic(err)
+    }
+
+    <-out // Ignore the first shell output, this is from setting up the session
+
+    // First we need to use the ECS agent introspection too to find the container ID that we require
+    // We will do this with a cURL call from inside the host
+    in <- fmt.Sprintf("curl http://localhost:51678/v1/tasks?arn=%s", c.TaskArn)
+    agentData := <-out
+
+    containerId := shellGetDockerId(string(agentData), c)
+
+    fmt.Println(containerId)
+
+    //<-out //ignore the shell output
+    //in <- "ls -lhav"
+    //fmt.Printf("ls output: %s\n", <-out)
+    //
+    in <- "whoami"
+    fmt.Printf("whoami: %s\n", <-out)
+
+    in <- "exit"
+    session.Wait()
 
     os.Exit(0)
 }
 
-func shellGetDockerId (session *ssh.Session, c ContainerDigest) string {
-    curlCommand := fmt.Sprintf("curl http://localhost:51678/v1/tasks?arn=%s", c.TaskArn)
-    out, err := session.CombinedOutput(curlCommand)
-    if err != nil {
-        fmt.Println(err)
-    }
-
-    tasks := gjson.Get(string(out), "Tasks")
+func shellGetDockerId (json string, c ContainerDigest) string {
+    tasks := gjson.Get(string(json), "Tasks")
 
     if tasks.IsArray() {
         for _, task := range tasks.Array() {
@@ -122,4 +159,39 @@ func shellGetDockerId (session *ssh.Session, c ContainerDigest) string {
     }
 
     return ""
+}
+
+func MuxShell(w io.Writer, r io.Reader) (chan<- string, <-chan string) {
+    in := make(chan string, 1)
+    out := make(chan string, 1)
+    var wg sync.WaitGroup
+    wg.Add(1) // for the shell itself
+    go func() {
+        for cmd := range in {
+            wg.Add(1)
+            w.Write([]byte(cmd + "\n"))
+            wg.Wait()
+        }
+    }()
+    go func() {
+        var (
+            buf [65 * 1024]byte
+            t   int
+        )
+        for {
+            n, err := r.Read(buf[t:])
+            if err != nil {
+                close(in)
+                close(out)
+                return
+            }
+            t += n
+            if buf[t-2] == '$' { // assuming the $PS1 == 'sh-4.3$ '
+                out <- string(buf[:t])
+                t = 0
+                wg.Done()
+            }
+        }
+    }()
+    return in, out
 }
